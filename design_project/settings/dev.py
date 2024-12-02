092b2062e119e251ab2f57afecc1bebebee3ae7c
mustafaAIM <moustafa.alhaiba.2003@gmail.com>
2024-12-02 17:34:52 +0300

    refactor: Simplify CORS handling
    Remove redundant CORS configuration from Nginx.
    
    Delegate all CORS management to Django.
    
    Improve maintainability and clarity.

--- design_project/settings/dev.py
+++ design_project/settings/dev.py
@@ -13,30 +13,7 @@ MEDIA_ROOT = '/var/www/media/'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "http://45.9.191.191",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
-
 CORS_PREFLIGHT_MAX_AGE = 86400
-
-CORS_ALLOW_METHODS = [
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
-]
-
-CORS_ALLOW_HEADERS = [
-    'accept',
-    'accept-encoding',
-    'authorization',
-    'content-type',
-    'dnt',
-    'origin',
-    'user-agent',
-    'x-csrftoken',
-    'x-requested-with',
-]
