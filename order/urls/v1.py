529e9a3a81d78e57d5c6171c2615157ce001e780
mustafaAIM <moustafa.alhaiba.2003@gmail.com>
2024-12-18 15:57:53 +0300

    add order and cart item count

--- order/urls/v1.py
+++ order/urls/v1.py
@@ -2,11 +2,12 @@ from django.urls import path
 from order.views import OrderViewSet
 
 urlpatterns = [
+    path('orders/', OrderViewSet.as_view({'get': 'list'}), name='order-list'),
+    path('orders/count/', OrderViewSet.as_view({'get': 'count'}), name='order-count'),
     path('order/create/', OrderViewSet.as_view({'post': 'create_order'}), name='order-create'),
     path('order/<uuid:uuid>/', OrderViewSet.as_view({
         'get': 'retrieve',
         'patch': 'partial_update',
         'delete': 'destroy'
     }), name='order-detail'),
-    path('orders/', OrderViewSet.as_view({'get': 'list'}), name='order-list'),
 ] 
