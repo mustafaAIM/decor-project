0808be3c5f2a9476074ffca77cf53284ae3ed398
Sami Alomar <sami.anas.alomar.2016.king@gmail.com>
2024-12-24 17:49:29 +0300

    added color-list setting the nginx server to the remote server

--- product/urls/v1.py
+++ product/urls/v1.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 # views
 from ..views.product_view import ProductViewSet
 from ..views.rate_view import RateViewSet , generate_product_report
+from ..views.color_view import ColorListView
 
 router = DefaultRouter()
 router.register(r'products', ProductViewSet)
@@ -11,4 +12,5 @@ router.register(r'rate', RateViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('report/', generate_product_report, name='product_report'),
+    path('colors/', ColorListView.as_view(), name='color-list'),
 ]
