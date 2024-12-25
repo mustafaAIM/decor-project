f905f115b2d69cd95552808a76a891b17e1dfe10
mustafaAIM <moustafa.alhaiba.2003@gmail.com>
2024-12-26 00:28:47 +0300

    add consulting service as well free slot getting time

--- service/urls/v1.py
+++ service/urls/v1.py
@@ -3,11 +3,13 @@ from rest_framework.routers import DefaultRouter
 from ..views.design_service_views import DesignServiceViewSet
 from ..views.service_settings_views import ServiceSettingsViewSet
 from ..views.area_service_views import AreaServiceViewSet
+from ..views.consulting_service_views import ConsultingServiceViewSet
 
 router = DefaultRouter()
 router.register(r'design-services', DesignServiceViewSet, basename='design-service')
 router.register(r'service-settings', ServiceSettingsViewSet, basename='service-settings')
 router.register(r'area-services', AreaServiceViewSet, basename='area-service')
+router.register(r'consulting-services', ConsultingServiceViewSet, basename='consulting-service')
 
 urlpatterns = [
     path('', include(router.urls)),
