37cc3e5f1ab2a527ca6898da143b744fd2b619c2
mustafaAIM <moustafa.alhaiba.2003@gmail.com>
2024-12-26 20:28:26 +0300

    add data

--- service/views/service_method_views.py
+++ service/views/service_method_views.py
@@ -5,6 +5,7 @@ from ..models.service_method_model import ServiceMethod
 from ..serializers.service_method_serializer import ServiceMethodSerializer
 from admin.permissions import IsAdmin
 from rest_framework.permissions import IsAuthenticated
+from utils import ResponseFormatter
 
 class ServiceMethodViewSet(viewsets.ModelViewSet):
     serializer_class = ServiceMethodSerializer
@@ -18,9 +19,33 @@ class ServiceMethodViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return ServiceMethod.objects.all()
 
+    def list(self, request, *args, **kwargs):
+        queryset = self.get_queryset()
+        serializer = self.get_serializer(queryset, many=True)
+        return ResponseFormatter.success_response(data=serializer.data)
+
+    def retrieve(self, request, *args, **kwargs):
+        instance = self.get_object()
+        serializer = self.get_serializer(instance)
+        return ResponseFormatter.success_response(data=serializer.data)
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        return ResponseFormatter.success_response(data=serializer.data)
+
+    def update(self, request, *args, **kwargs):
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return ResponseFormatter.success_response(data=serializer.data)
+
     @action(detail=False, methods=['get'])
     def available(self, request):
         """Get only available service methods"""
         methods = ServiceMethod.objects.filter(is_available=True)
         serializer = self.get_serializer(methods, many=True)
-        return Response(serializer.data) 
+        return ResponseFormatter.success_response(data=serializer.data) 
