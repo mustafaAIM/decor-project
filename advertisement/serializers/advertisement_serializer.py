3891d629f8f2a2b01fac8a5ef63d70e756be6d00
mustafaAIM <moustafa.alhaiba.2003@gmail.com>
2025-01-25 01:23:27 +0300

    add arabic

--- advertisement/serializers/advertisement_serializer.py
+++ advertisement/serializers/advertisement_serializer.py
@@ -4,5 +4,13 @@ from ..models import Advertisement
 class AdvertisementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Advertisement
-        fields = ['uuid', 'title', 'description', 'image', 'created_at']
+        fields = [
+            'uuid', 
+            'title_ar', 
+            'title_en', 
+            'description_ar', 
+            'description_en', 
+            'image', 
+            'created_at'
+        ]
         read_only_fields = ['uuid', 'created_at'] 
